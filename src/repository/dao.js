6f250a0e1314eb6b7f53a47737c2a6a2911ba896
kpi-jps <jps.spj@gmail.com>
2022-10-12 23:53:16 -0300

    under construction

--- src/repository/dao.js
+++ src/repository/dao.js
@@ -38,7 +38,7 @@ const checkUserCredentials = async (email, passwd) => {
 
 const updateUserName = async (email, name) => {
     await db.sync();
-    const user = await models.User.update({name: name}, {
+    await models.User.update({name: name}, {
         where : {
             email : email
         }
@@ -48,7 +48,7 @@ const updateUserName = async (email, name) => {
 const updateUserPasswd = async (email, newPasswd) => {
     await db.sync();
     bcrypt.hash(newPasswd, saltRounds, (err, hash) => {
-        const user = models.User.update({hash: hash}, {
+        models.User.update({hash: hash}, {
             where : {
                 email : email
             }
@@ -56,8 +56,56 @@ const updateUserPasswd = async (email, newPasswd) => {
     });
 }
 
-const createCryptoRegistry = async (ticker, value, userId) => {
+const findCryptoRegistry = async (id, userId) => {
+    await db.sync();
+    const cryptoRegistry = await models.User.findOne({
+        where: {
+            id: id,
+            userId: userId
+        }
+    });
+   return cryptoRegistry;
+}
 
+const findCryptoRegisters = async (userId) => {
+    await db.sync();
+    const cryptoRegisters = await models.User.findAll({
+        where: {
+            userId: userId
+        }
+    });
+   return cryptoRegisters;
+}
+
+const createCryptoRegistry = async (ticker, value, userId) => {
+    await db.sync();
+    const cryptoRegistry = await models.CryptoRegistry.create({
+        ticker: ticker,
+        value: value,
+        userId: userId
+    })
+}
+
+
+const updateCryptoRegistry = async (ticker, value, userId) => {
+    await db.sync();
+    await models.CryptoRegistry.update({
+        ticker: ticker,
+        value: value
+    }, { where: {
+        userId: userId
+    }})
+}
+
+const deleteCryptoRegistry = async (id, userId) => {
+    await db.sync();
+    await models.CryptoRegistry.destroy({
+        where: {
+            id: id,
+            userId: userId
+        }
+    })
 }
 //createUser('João Pedro', 'jps.spj@gmail.com', '1234');
+deleteCryptoRegistry(1,1);
 console.log(checkUserCredentials('jps.spj@gmail.com', '1234'));
